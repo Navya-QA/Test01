f7b8ad8c6b7fe499f194164227cef89ad745ff3f
Navyats21 <tsnavya21@gmail.com>
2024-08-04 18:17:40 +0530

    updated

--- ExpenseApp/FrontEnd.sh
+++ ExpenseApp/FrontEnd.sh
@@ -1,6 +1,19 @@
+set-hostnmame FrontEnd
 dnf install nginx 
 systemctl enable nginx 
 systemctl restart nginx
+rm -rf /usr/share/nginx/html/*   
+curl -o /tmp/frontend.zip https://expense-web-app.s3.amazonaws.com/frontend.zip
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
 systemctl status nginx
 
 echo " installed successfully !! "
+
+cd ..
+cd ..
+
+cp pro.conf /etc/nginx/default.d/expense.conf 
+systemctl restart nginx
+systemctl status nginx
+
